33a4718ac9fd912714c6a13b5ae1e6bcdad0f536
Daham Drake Wang <drakewang@gmail.com>
2017-12-25 19:42:21 -0800

    Complete sending test

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,3 +1,19 @@
 var app = {
-  init: function () {},
+  init: function () {
+    console.log('app init');
+  },
+  send: function(message) {
+    $.ajax({
+      type: 'POST',
+      url: 'http://127.0.0.1:3000/classes/messages',
+      data: JSON.stringify(message),
+      success: function() {
+        console.log('success');
+      },
+      error: function(err) {
+        console.log(err);
+      },
+      dataType: 'json'
+    });
+  },
 };
