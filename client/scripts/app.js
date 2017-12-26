258cc6d877c377c633e182fcd9e78e9957131024
Daham Drake Wang <drakewang@gmail.com>
2017-12-25 20:06:58 -0800

    Complete fetching test

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,12 @@
 var app = {
+  server: 'http://127.0.0.1:3000/classes/messages',
   init: function () {
     console.log('app init');
   },
   send: function(message) {
     $.ajax({
       type: 'POST',
-      url: 'http://127.0.0.1:3000/classes/messages',
+      url: app.server,
       data: JSON.stringify(message),
       success: function() {
         console.log('success');
@@ -16,4 +17,16 @@ var app = {
       dataType: 'json'
     });
   },
+  fetch: function() {
+    $.ajax({
+      type: 'GET',
+      url: app.server,
+      success: function() {
+        console.log('success get');
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    });
+  }
 };
